46f822521183c9615d708588883d886453173b19
SebastianAvila <jesusaviala@gmail.com>
2023-10-29 11:38:29 -0600

    finalizados

--- src/components/WriteData.vue
+++ src/components/WriteData.vue
@@ -7,7 +7,10 @@
             Datos
             <v-spacer></v-spacer>
           </v-card-title>
-          <v-data-table :headers="headers" :items="Records" :items-per-page="5" :footer-props="{
+
+          <v-skeleton-loader v-if="loading" type="table"></v-skeleton-loader> <!-- Skeleton Loader para la tabla -->
+          <!-- Mostrar la tabla cuando loading sea false -->
+          <v-data-table v-if="!loading" :headers="headers" :items="Records" :items-per-page="5" :footer-props="{
             showFirstLastPage: true,
             firstIcon: 'mdi-arrow-collapse-left',
             lastIcon: 'mdi-arrow-collapse-right',
@@ -30,8 +33,11 @@
           </v-data-table>
         </v-card>
       </v-col>
+
       <v-dialog v-model="dialog" width="800">
-        <v-card>
+        <v-skeleton-loader v-if="loading" type="text"></v-skeleton-loader> <!-- Skeleton Loader para el v-dialog -->
+
+        <v-card v-if="!loading"> <!-- Mostrar el v-dialog cuando loading sea false -->
           <v-card-title class="text-h5 grey lighten-2">
             Resultados de la solicitud
           </v-card-title>
@@ -46,7 +52,6 @@
             <v-text-field label="IncludeIvaTransferred" v-model="responseData.IncludeIvaTransferred"
               disabled></v-text-field>
             <v-text-field label="ProdServCode" v-model="responseData.ProdServCode" disabled></v-text-field>
-
           </v-card-text>
           <v-divider></v-divider>
           <v-card-actions>
@@ -65,6 +70,7 @@
 export default {
   data() {
     return {
+      loading: true, // Agregamos la variable de carga
       headers: [
         {
           text: 'ProdServCode',
@@ -96,16 +102,15 @@ export default {
         IncludeIepsTransferred: "",
         ProdServCode: "",
       },
-
       dialog: false
     }
   },
   created() {
-    //Carga la funcion
+    //Carga la función
     this.getData();
   },
   methods: {
-    // Obtiene la data que se muestra al cargar la pagina 
+    // Obtiene la data que se muestra al cargar la página 
     async getData() {
       const urlGet = "https://services.schoolmanager.education/v1/sat/satproductserv/5251/10"
       try {
@@ -113,6 +118,7 @@ export default {
         if (response.ok) {
           const data = await response.json();
           this.Records = data.Records;
+          this.loading = false; // Desactivamos la variable de carga
         } else {
           console.error('Error al obtener datos:', response.statusText);
         }
@@ -120,7 +126,7 @@ export default {
         console.error('Error al obtener datos:', error);
       }
     },
-    // Obtiene la data al hacer click consumientdo la otra API
+    // Obtiene la data al hacer clic consumiendo la otra API
     async getSecondData(item) {
       this.dialog = true;
       // Estructura necesaria para obtener el GET haciendo POST
@@ -148,26 +154,23 @@ export default {
         if (response.ok) {
           const responseData = await response.json();
           //console.log(responseData)
-          //elementos fuera 
+          //Elementos fuera 
           this.responseData.Code = responseData.Code;
           this.responseData.CurrentPage = responseData.CurrentPage;
           this.responseData.Message = responseData.Message;
-          // Accede a IdSatCode dentro de Records y lo asígna
+          // Accede a IdSatCode dentro de Records y lo asigna
           if (responseData.Records && responseData.Records.length > 0) {
             const record = responseData.Records[0];
             // Elementos internos
-
             this.responseData.Description = record.Description;
             this.responseData.IdSatCode = record.IdSatCode;
             this.responseData.IncludeIvaTransferred = record.IncludeIvaTransferred;
             this.responseData.IncludeIepsTransferred = record.IncludeIepsTransferred;
             this.responseData.ProdServCode = record.ProdServCode;
           } else {
-            // Si no hay datos en Records, puedes mostrar un mensaje de error
+            // Si no hay datos en Records, muestra un mensaje de error
             console.error("No se encontraron datos en Records");
           }
-
-
         } else {
           console.error('Error al realizar la solicitud:', response.statusText);
         }
